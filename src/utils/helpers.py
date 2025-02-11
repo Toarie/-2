0a246361f27e7ee9911659e01c8ec0f575e6fb8d
Ulyana Kuznetsova <rey.tsvetkov777@gmail.com>
2025-02-11 17:49:27 +0300

    незначительные изменения

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,22 @@
-def filter_vacancies(vacancies, filter_words):
-    return [vacancy for vacancy in vacancies if any(word in vacancy.description for word in filter_words)]
+from typing import List
+from src.models.vacancy import Vacancy
 
-def sort_vacancies(vacancies):
-    return sorted(vacancies, reverse=True)
+def filter_vacancies(vacancies: List[Vacancy], filter_words: List[str]) -> List[Vacancy]:
+    """
+    Фильтрует список вакансий по ключевым словам в описании.
+
+    :param vacancies: Список вакансий.
+    :param filter_words: Список ключевых слов для фильтрации.
+    :return: Отфильтрованный список вакансий.
+    """
+    return [vacancy for vacancy in vacancies if any(word.lower() in vacancy.description.lower() for word in filter_words)]
+
+def sort_vacancies(vacancies: List[Vacancy]) -> List[Vacancy]:
+    """
+    Сортирует список вакансий. По умолчанию сортирует по убыванию (например, по зарплате или дате).
+
+    :param vacancies: Список вакансий.
+    :return: Отсортированный список вакансий.
+    """
+    # Пример сортировки по зарплате (предполагается, что salary является числом или имеет метод сравнения)
+    return sorted(vacancies, key=lambda x: x.salary, reverse=True)
